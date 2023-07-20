9c87a2d600bcd5b6ccac4486d9429e314b36b92a
DUKT Squad - SHYNZ Development Team <duket@shynz.com>
2023-07-19 20:53:43 -0400

    First Commit

---
+++ const/addresses.ts
@@ -0,0 +1,3 @@
+export const LOTTERY_CONTRACT_ADDRESS = '0xCe8A98971C9E65f2649f173E6D46Fd50d14dc32c';
+
+export const HERO_IMAGE_URL = 'ipfs://QmS1gz2fNL6DfnfebNsY3KkdYh3vT4jcEArkndao6eCk5Z/Untitled%20design%20(14).png';
